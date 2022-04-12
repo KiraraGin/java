d489f40a4d4dc0dfb6d6a4a46636b93bd848ebfe
kitahara <kita@asojuku.ac.jp>
2022-04-12 16:00:21 +0900

    メッセージを追加
    println

--- ThreadSample/MyThread.java
+++ ThreadSample/MyThread.java
@@ -2,5 +2,6 @@ public class MyThread extends Thread {
     public void run() {
         String threadName = Thread.currentThread().getName();
         System.out.println("スレッド" + threadName);
+	System.out.println("Changed");
     }
 }
